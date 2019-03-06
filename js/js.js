b89c467741c2d80acc070b68c89ce863613b404c
olya-sobaka <46378189+olya-sobaka@users.noreply.github.com>
2019-03-06 17:12:10 +0100

    Делает кнопку открытия формы ссылкой на внутреннюю страницу

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 var button=document.querySelector(".search-form-toggle");
     var popup=document.querySelector(".hotel-search-form");
     button.addEventListener("click", function(evt) {
+      evt.preventDefault();
       popup.classList.toggle("hotel-search-show");
     })
